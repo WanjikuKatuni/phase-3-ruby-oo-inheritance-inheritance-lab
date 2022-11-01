0322debd3dc487827ab06a4f789dea5f87c71dc1
wanjiku <annritawanjiku77@gmail.com>
2022-11-01 08:15:40 +0300

    complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    # @@knowledge = []
+
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+    def learn(new_knowledge)
+        @knowledge << new_knowledge
+     end
+     def knowledge
+        return @knowledge
+    end
 end
